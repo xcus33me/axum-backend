1249d1889329dd555d7d2fbf0ce74ba39ef51e59
xcus33me <HarvieOrigin@yandex.ru>
2024-11-14 22:46:51 +0300

    feat: added role_check function

--- src/middleware.rs
+++ src/middleware.rs
@@ -8,7 +8,7 @@ use axum::{
     Extension,
 };
 
-use axum_extra::extract::cookie::{Cookie, CookieJar};
+use axum_extra::extract::cookie::CookieJar;
 use serde::{Serialize, Deserialize};
 
 use crate::{
@@ -31,20 +31,20 @@ pub async fn auth(
     next: Next,
 ) -> Result<impl IntoResponse, HttpError> {
     let cookies = cookie_jar
-        .get("token")
-        .map(|cookie| cookie.value().to_string())
-        .or_else(|| {
-            req.headers()
-                .get(header::AUTHORIZATION)
-                .and_then(|auth_header| auth_header.to_str().ok())
-                .and_then(|auth_value| {
-                    if auth_value.starts_with("Bearer ") {
-                        Some(auth_value[7..].to_owned())
-                    } else {
-                        None
-                    }
-                })
-        });
+            .get("token")
+            .map(|cookie| cookie.value().to_string())
+            .or_else(|| {
+                req.headers()
+                    .get(header::AUTHORIZATION)
+                    .and_then(|auth_header| auth_header.to_str().ok())
+                    .and_then(|auth_value| {
+                        if auth_value.starts_with("Bearer ") {
+                            Some(auth_value[7..].to_owned())
+                        } else {
+                            None
+                        }
+                    })
+            });
 
     let token = cookies.ok_or_else(|| {
         HttpError::unauthorized(ErrorMessage::TokenNotProvided.to_string())
@@ -59,15 +59,15 @@ pub async fn auth(
         };
 
     let user_id = uuid::Uuid::parse_str(&token_details.to_string())
-        .map_err(|_| {
-            HttpError::unauthorized(ErrorMessage::InvalidToken.to_string())
-        })?;
+            .map_err(|_| {
+                HttpError::unauthorized(ErrorMessage::InvalidToken.to_string())
+            })?;
 
     let user = app_state.db_client.get_user(Some(user_id), None, None, None)
-        .await
-        .map_err(|_| {
-            HttpError::unauthorized(ErrorMessage::UserNoLongerExist.to_string())
-        })?;
+            .await
+            .map_err(|_| {
+                HttpError::unauthorized(ErrorMessage::UserNoLongerExist.to_string())
+            })?;
 
     let user = user.ok_or_else(|| {
         HttpError::unauthorized(ErrorMessage::UserNoLongerExist.to_string())
@@ -76,6 +76,26 @@ pub async fn auth(
     req.extensions_mut().insert(JWTAuthMiddleware {
         user: user.clone(),
     });
+
+    Ok(next.run(req).await)
+}
+
+pub async fn role_check(
+    Extension(_app_state): Extension<Arc<AppState>>,
+    req: Request,
+    next: Next,
+    required_roles: Vec<UserRole>,
+) -> Result<impl IntoResponse, HttpError> {
+    let user = req
+            .extensions()
+            .get::<JWTAuthMiddleware>()
+            .ok_or_else(|| {
+                HttpError::unauthorized(ErrorMessage::UserNotAuthenticated.to_string())
+            })?;
+
+    if !required_roles.contains(&user.user.role) {
+        return Err(HttpError::new(ErrorMessage::PermissionDenied.to_string(), StatusCode::FORBIDDEN));
+    }
 
     Ok(next.run(req).await)
 }
